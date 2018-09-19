0030f7c59942f5c8faa0b96363f487f4376a0229
eduardomatias <eduardomatias.1989@gmail.com>
2018-09-18 23:36:40 -0300

    set email de destino do contato

--- enviar.php
+++ enviar.php
@@ -1,24 +1,21 @@
 <?php
 
-  $nome = $_POST['nome'];
-  $email = $_POST['email'];
-  $mensagem = $nome . '<br />' . $email . '<br />' . $_POST['assunto'] . ': ' . $_POST['mensagem'];
+$nome = addslashes($_POST['nome']);
+$email = addslashes($_POST['email']);
+$mensagem = $nome . '<br />' . $email . '<br /><strong>' . addslashes($_POST['assunto']) . '</strong>: ' . addslashes($_POST['mensagem']);
 
-  // emails para quem será enviado o formulário
-  $emailenviar = "softmaxi@softmaxi.com.br";
-  $emailenviar = "eduardomatias.1989@gmail.com";
-  $destino = $emailenviar;
-  $assunto = "Contato pelo Site";
+// emails para quem será enviado o formulário
+$emailenviar = "softmaxi@softmaxi.com.br";
+$destino = $emailenviar;
+$assunto = "Contato pelo site (www.softmaxi.com.br)";
 
-  // É necessário indicar que o formato do e-mail é html
-  $headers  = 'MIME-Version: 1.0' . "\r\n";
-  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-  $headers .= 'From: $nome <$email>';
-  //$headers .= "Bcc: $EmailPadrao\r\n";
-  
-  $enviaremail = mail($destino, $assunto, $mensagem, $headers);
-  var_dump($enviaremail);
-  $retorno = ($enviaremail) ? : "ERRO AO TENTAR ENVIAR A MENSAGEM, TENTE NOVAMENTE!";
-  echo json_encode($retorno);
+// É necessário indicar que o formato do e-mail é html
+$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'From: ' . $nome . '<' . $email . '>';
+//$headers .= "Bcc: $EmailPadrao\r\n";
 
+$enviaremail = mail($destino, $assunto, $mensagem, $headers);
+$retorno = ($enviaremail) ? : "ERRO AO TENTAR ENVIAR A MENSAGEM, TENTE NOVAMENTE!";
+echo json_encode($retorno);
 ?>
