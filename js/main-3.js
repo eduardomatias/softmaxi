8be99760b590614b0dddcf4ad7e818295e041e10
eduardomatias <eduardomatias.1989@gmail.com>
2018-10-19 23:46:02 -0300

    add screens

--- js/main-3.js
+++ js/main-3.js
@@ -149,4 +149,53 @@
         htmlModal.load('segmentos/' + $(this).data('segmento') + '.html');
     });
 
+
+    /*----------------------------------------------------*/
+    /*  Owl Carousel - SCREENSHOTS */
+    /*----------------------------------------------------*/
+    $(".screenshots-slider").owlCarousel({
+        nav: false,
+        dots: true,
+        autoplay: true,
+        loop: false,
+        mouseDrag: true,
+        margin: 15,
+        touchDrag: true,
+        navText: ["<i class='icofont icofont-arrow-left circled-icon'></i>", "<i class='icofont icofont-arrow-right circled-icon'></i>"],
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 1,
+                padding: 30
+            },
+            // breakpoint from 480 up
+            480: {
+                items: 2,
+            },
+            // breakpoint from 600 up
+            600: {
+                items: 3,
+            },
+            // breakpoint from 1000 up
+            1000: {
+                items: 4,
+                dots: false,
+                margin: 0
+            }
+        }
+    });
+
+    /*----------------------------------------------------*/
+    /*  SCREENSHOT IMAGE POPUP */
+    /*----------------------------------------------------*/
+    $('.screenshot-overlay a').magnificPopup({
+        type: 'image',
+        removalDelay: 300,
+        mainClass: 'mfp-fade',
+        gallery: {
+            enabled: true
+        }
+    });
+
+
 }(jQuery));
